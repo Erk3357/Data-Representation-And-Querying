15229783963178208345d39f3e0be8cdf26927c3
Eryk Kowalewicz <49148214+Erk3357@users.noreply.github.com>
2021-10-12 08:21:02 +0000

    Initiate Node

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,37 +1,43 @@
-import React, { Component } from 'react';
 import './App.css';
-import Header from './components/header.js';
-import Footer from'./components/footer.js';
-import Content from'./components/content.js';
+//Import React Component from React library
+import React, { Component } from 'react';
+import Header from './Components/header';
+import Footer from './Components/footer';
+import Content from './Components/content';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar, Nav, Container} from 'react-bootstrap';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import {BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 
 class App extends Component{
+
   render(){
     return (
       <Router>
         <div className="App">
           <Navbar bg="light" expand="lg">
             <Container>
-              <Navbar.Brand href="/">movie-app: Eryk</Navbar.Brand>
+              <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                  <Nav.Link href="/">Content</Nav.Link>
-                  <Nav.Link href="/header">Header</Nav.Link>
-                  <Nav.Link href="/footer">Footer</Nav.Link>
+                  {/* Create Navs that will replace the componets via user root*/}
+                  <Nav.Link href="/">Home</Nav.Link>
+                  <Nav.Link href="/header">header</Nav.Link>
+                  <Nav.Link href="/footer">footer</Nav.Link>
                 </Nav>
               </Navbar.Collapse>
             </Container>
           </Navbar>
 
+          {/*Switch Between different componetns (guide for Router*/}
           <Switch>
-            <Route path='/' component={Content} exact/>
-            <Route path='/header' component={Header} exact/>
-            <Route path='/footer' component={Footer} exact/>
-          </Switch>  
-
+            <Route path='/' component={Content} exact></Route>
+            <Route path='/header' component={Header}></Route>
+            <Route path='/footer' component={Footer}></Route>
+          </Switch>
         </div>
       </Router>
     );
