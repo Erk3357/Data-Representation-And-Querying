fa0860a081223cbf7a336c177f703e13293e4c10
Eryk Kowalewicz <49148214+Erk3357@users.noreply.github.com>
2021-11-16 10:50:19 +0000

    Initiate Lab 8

---
+++ Lab 8/movie-app/src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './components/header';
+import Content from './components/content';
+import Footer from './components/footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Read from './components/read';
+import Create from './components/create';
+import { connect } from 'mongoose';
+import Edit from './components/edit';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      <div className="App">
+        <Navbar bg="primary" variant="dark">
+          <Container>
+            <Navbar.Brand href="/">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/read">Read</Nav.Link>
+              <Nav.Link href="/create">Create</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <Switch>
+          <Route path="/" component={Content} exact />
+          <Route path="/create" component={Create} />
+          <Route path="/read" component={Read} />
+          <Route path="/edit/:id" component={Edit} />
+        </Switch>
+      </div>
+      </Router>
+    );
+  }
+}
+export default App;
